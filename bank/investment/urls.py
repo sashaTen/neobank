1c4f37d227c022ab9cb25654393d317e1af359b7
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-08-01 11:45:20 +0900

    added templates

--- bank/investment/urls.py
+++ bank/investment/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('/about' ,  views.aboutUs ,  name  = 'about'),
-    path('/planner' ,   views.plansDaylyAnalysis ,   name  = 'planning')
+   
    
 ]   
--- bank/investment/views.py
+++ bank/investment/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def welcome(request):
-    return HttpResponse('welcome page . start investing  and  bring   your   portfolio')
+    return render(request ,  'welcome.html')
 
 
 
 def aboutUs(request):
-    return  HttpResponse('our team gives  the  best  analysis  of your portfolio using the state of art techniques')
+    return  render(request  ,  'about.html')
 
 
-def plansDaylyAnalysis(request):
-    return  HttpResponse('today i did   :  1   python startapp invest   2 regsiter in main setting the app 3   include urls of app in main  4 urls with propoer / 5 wrote basic views .    my plan  for  future  ')
