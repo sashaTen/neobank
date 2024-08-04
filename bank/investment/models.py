c6ba8b4fca35b77ecfa24b15e3825be78281b0e9
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-08-04 19:32:24 +0900

    admin side of working with models

--- bank/investment/models.py
+++ bank/investment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Inverstor(models.Model):
+    name =  models.CharField(max_length=200)
+    phone_number = models.IntegerField(max_length=15)
+    joined_date =models.DateField()
+    class Meta : 
+        db_table = 'investor'
+    def __str__(self):
+        return f"Employee(Name: {self.name})"
+
