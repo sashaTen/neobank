c6fc20b7acfc4a90db019ae9ef56c1c410ec19fb
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-08-06 16:25:00 +0900

    added   email to   investor model

--- bank/investment/forms.py
+++ bank/investment/forms.py
@@ -4,4 +4,4 @@ from .models import Investor
 class InvestorForm(forms.ModelForm):
     class Meta:
         model = Investor
-        fields = ['name', 'phone_number']
+        fields = [ 'email','name', 'phone_number']
--- bank/investment/models.py
+++ bank/investment/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Investor(models.Model):
+    email =  models.CharField(max_length=100 ,   default='SOME STRING')
     name =  models.CharField(max_length=200)
     phone_number = models.IntegerField(max_length=15)
 
