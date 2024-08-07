5f2ea574042ae3dc1381a46d105f94204ed815fe
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-08-07 18:18:53 +0900

    updated investor info

--- bank/investment/forms.py
+++ bank/investment/forms.py
@@ -4,4 +4,4 @@ from .models import Investor
 class InvestorForm(forms.ModelForm):
     class Meta:
         model = Investor
-        fields = [ 'email','name', 'phone_number']
+        fields = [ 'email','name', 'phone_number' , 'risk_tolerance' , 'investment_horizon', 'initial_investment' , 'income' , 'financial_goals']
--- bank/investment/models.py
+++ bank/investment/models.py
@@ -5,6 +5,12 @@ class Investor(models.Model):
     email =  models.CharField(max_length=100 ,   default='SOME STRING')
     name =  models.CharField(max_length=200)
     phone_number = models.IntegerField(max_length=15)
+    risk_tolerance = models.CharField(max_length=50, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')] ,   default ='low')
+    investment_horizon = models.IntegerField(max_length=100 ,default=0  )  # in years
+    initial_investment = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    income = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    financial_goals = models.TextField(default='SOME STRING')
+
 
     class Meta : 
         db_table = 'investor'
